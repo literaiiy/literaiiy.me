390f9ba65fd336af812372ab3578a252ff14cbec
Ethan Yim <liddery69@gmail.com>
2021-09-04 15:18:31 -0700

    lastfm and github icons finally

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-
-// const lastFm = <FontAwesomeIcon icon={faCoffee} />;
 
 class Navbar extends React.Component {
   render() {
     return (
       <nav>
-        <FontAwesomeIcon icon={faCoffee} />
+        <a href="https://github.com/literaiiy"><img height="32" width="32" src="https://unpkg.com/simple-icons@v5/icons/github.svg" /></a>
+        <a href="https://last.fm/user/literaiiy"><img height="32" width="32" src="https://unpkg.com/simple-icons@v5/icons/lastdotfm.svg" /></a>
       </nav>
     )
   }
