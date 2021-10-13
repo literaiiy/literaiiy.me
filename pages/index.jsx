1ab7aef28ffe9b646fdd6f320168eff8a0272a48
Ethan Yim <liddery69@gmail.com>
2021-10-12 19:46:27 -0700

    HTML semantic improvements part 1

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,37 +15,6 @@ export default class Home extends Component {
     return (
       <div> 
         {/* START */}
-        <Head>
-          {/* Title */}
-          <title>Home - literaiiy.me</title>
-          {/* Stylesheets */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&family=Satisfy&family=Norican" />
-          <link rel="canonical" href="https://literaiiy.me" />
-          {/* Meta tags */}
-          <meta name="title" content="Home - literaiiy.me"/>
-          <meta name="description" content="Hello! I am literaiiy (styled lowercase), a smartphone, web development & design, computer, and many-other-things enthusiast residing in the Bay Area."/>
-          <meta name="keywords" content="literaiiy, toolbox, tiramisu, dah.li/a"/>
-          <meta name="robots" content="index, follow"/>
-          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-          <meta name="language" content="English"/>
-          <meta name="author" content="literaiiy"/>
-            {/* OpenGraph */}
-            <meta property="og:title" content="Home - literaiiy.me" />
-            <meta property="og:site_name" content="literaiiy.me" />
-            <meta property="og:url" content="https://literaiiy.me" />
-            <meta property="og:description" content="Hello! I am literaiiy (styled lowercase), a smartphone, web development & design, computer, and many-other-things enthusiast residing in the Bay Area." />
-            <meta property="og:type" content="profile" />
-            <meta property="og:image" content="https://user-images.githubusercontent.com/64048778/136896416-258d2b8d-50b8-4983-8888-3fe2e4c1453f.png" />
-            {/* Twitter */}
-            <meta name="twitter:card" content="summary" />
-            <meta name="twitter:title" content="Home - literaiiy.me" />
-            <meta name="twitter:description" content="Hello! I am literaiiy (styled lowercase), a smartphone, web development & design, computer, and many-other-things enthusiast residing in the Bay Area."/ >
-            <meta name="twitter:image" content="https://user-images.githubusercontent.com/64048778/136896416-258d2b8d-50b8-4983-8888-3fe2e4c1453f.png"/>
-          {/* Scripts & Libraries */}
-          <script src="https://cdn.jsdelivr.net/npm/simple-parallax-js@5.5.1/dist/simpleParallax.min.js"></script>
-        </Head>
         {/* <ScrollBarTop/> */}
         <Navbar/>
         <main>
@@ -98,7 +67,7 @@ export default class Home extends Component {
               <p>
                 literaiiy is a pseudonym I gave myself back in spring of 2018 as an intentionally spelt version
                 of the word "literally". While appearing frequently in everyday conversation, it is 
-                not usually used correctly strictly by definition, but more as hyperbole; a common word that has dulled due to 
+                not usually used correctly strictly by definition, but more as hyperbole— a common word that has dulled due to 
                 overuse.
               </p>
               <h2>
