81de52ddf990d183108d78959a85a8401c7b13cf
Ethan Yim <liddery69@gmail.com>
2021-10-16 16:01:21 -0700

    Optimization & CSS improvements for cross-platform compatibility

--- components/logo.jsx
+++ components/logo.jsx
@@ -7,37 +7,6 @@ class Logo extends React.Component {
     super(props)
   }
 
-  // componentDidMount() {
-  //   setTimeout(() => {
-  //     dotMe.textContent = ".com";
-  //     dotMe.style.fontFamily = 'Yesteryear';
-  //     setTimeout(() => {
-  //       dotMe.textContent = ".xyz";
-  //       dotMe.style.fontFamily = 'Nanum Brush Script';
-  //       setTimeout(() => {
-  //         dotMe.textContent = ".net";
-  //         dotMe.style.fontFamily = 'Dancing Script';
-  //         setTimeout(() => {
-  //           dotMe.textContent = ".us";
-  //           dotMe.style.fontFamily = 'Alex Brush';
-  //           setTimeout(() => {
-  //             dotMe.textContent = ".io";
-  //             dotMe.style.fontFamily = 'Arizonia';
-  //             setTimeout(() => {
-  //               dotMe.textContent = ".eu";
-  //               dotMe.style.fontFamily = 'Kristi';
-  //               setTimeout(() => {
-  //                 dotMe.textContent = ".me";
-  //                 dotMe.style.fontFamily = 'Birthstone Bounce';
-  //               }, 650)
-  //             }, 200)
-  //           }, 100)
-  //         }, 75)
-  //       }, 75)
-  //     }, 100)
-  //   }, 75)
-  // }
-
   render() {
     return (
       <h1 className='fpLogo'>
---
+++ pages/_error.jsx
@@ -0,0 +1,17 @@
+function Error({ statusCode }) {
+  return (
+    <div class="error">
+      <title>{`${statusCode} - literaiiy.me`}</title>
+      <h1>{statusCode}</h1>
+      <p>{(("" + statusCode).startsWith("5")) ? "A server error has occured. Try again later." : "This page doesn't seem to exist. Try again with a different URL."}</p>
+      <img class='errorImg' src="https://gist.githubusercontent.com/literaiiy/3e9da522838de94a099db49153742afe/raw/27571aa065655adf7b4dbc1e7f82cfb33840c9ba/birds.svg" alt="" />
+    </div>
+  )
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+  return { statusCode }
+}
+
+export default Error
