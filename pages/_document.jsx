27f42c4b3859cbd2ad1c2187b1d4c3f716f9cd7a
Ethan Yim <liddery69@gmail.com>
2021-10-23 18:54:01 -0700

    GTAG?

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,6 +7,18 @@ class DOM extends Document {
         <Head>
           {/* Title */}
           <title>Home - literaiiy.me</title>
+          {/* Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-8M18TPXGVR"></script>
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+  
+              gtag('config', 'G-8M18TPXGVR');`
+          }}>
+
+          </script>
           {/* Stylesheets */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
