32a8d11a1af70a6c34cd85aa48cd25e231db0d99
Ethan Yim <liddery69@gmail.com>
2021-10-23 18:05:34 -0700

    CSS normalizer used, W3C compliance update

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -12,6 +12,7 @@ class DOM extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&family=Satisfy&family=Norican&display=swap" />
           <link rel="canonical" href="https://literaiiy.me" />
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css" />
           {/* Meta tags */}
           <meta name="title" content="Home - literaiiy.me"/>
           <meta name="description" content="Hello! I am literaiiy (styled lowercase), a smartphone, web development & design, computer, and many-other-things enthusiast residing in the Bay Area."/>
@@ -37,11 +38,10 @@ class DOM extends Document {
         <body>
           <Main />
           <NextScript />
-        </body>
           {/* Scripts & Libraries */}
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.min.js"></script>
-          <script src="/js/curDot.js"></script>
-          <script src="/js/js.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.min.js"></script>
+            <script src="/js/js.js"></script>
+        </body>
       </Html>
     )
   }
