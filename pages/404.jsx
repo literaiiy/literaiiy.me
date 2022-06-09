9b977fccf51f83ed756a1abcd96a435e8b9cdb6d
Alex Wang <unixfy@users.noreply.github.com>
2022-06-09 18:32:35 -0400

    Add oops to 404 page
    This is very necessary.

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,7 +3,7 @@ export default function Custom404() {
     <div class="error">
       <title>404 - literaiiy.me</title>
       <h1>404</h1>
-      <p>This page doesn't seem to exist. Try again with a different URL or go <a href="/" style={{textDecoration: "underline",}}>back</a>.</p>
+      <p>Oops! This page doesn't seem to exist. Try again with a different URL or go <a href="/" style={{textDecoration: "underline",}}>back</a>.</p>
       <img class='errorImg' src="https://gist.githubusercontent.com/literaiiy/3e9da522838de94a099db49153742afe/raw/27571aa065655adf7b4dbc1e7f82cfb33840c9ba/birds.svg" alt="birds" />
   </div>
   )
